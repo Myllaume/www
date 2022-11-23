72fa93134ee3bd6f3ac1a5714d904b0d9e7aa663
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2022-11-23 18:36:23 +0100

    rework

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 const yaml = require("js-yaml");
 
 module.exports = function(e) {
-
     // copy 'assets' folder
-    e.addPassthroughCopy("assets");
+    e.addPassthroughCopy("src/assets");
     // read YAML data from 'data' folder
-    e.addDataExtension("yml", contents => yaml.safeLoad(contents));
+    e.addDataExtension("yml", contents => yaml.load(contents));
 
     return {
         dir: {
+            input: "src",
             output: "build",
             includes: "includes",
             layouts: "layouts",
---
+++ src/index.md
@@ -0,0 +1,9 @@
+---
+layout: basic.njk
+title: À propos
+tags: page
+---
+
+Je suis développeur, spécialisé dans les applications JavaScript documentaires, de gestion et représentation de la connaisssance.
+
+Je suis développeur de plusieurs [projets open source](https://github.com/graphlab-fr). Depuis avril 2020, j’ai intégré le [groupe de recherche Graphlab](https://www.graphlab.fr/a-propos/).
